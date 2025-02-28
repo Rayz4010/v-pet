171aa0a483733d0e135b781867f233c10994ee0a
rayz <rayquaza4010@icloud.com>
2025-03-01 00:27:12 +0530

    Set to go

--- virtual-pet/scripts/idle/happy_idle.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-var idle_path=$AnimatedSprite2D
-
-func _ready() -> void:
-	idle_path.play("AnimatedSprite2D")
-	
--- virtual-pet/scripts/idle/ill.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatedSprite2D
-var r= randf_range(0,1)
-func _ready() -> void:
-	if (r<0.9):
-		play("ill 1")
-	else:
-		play("ill 2")
-		
-		
